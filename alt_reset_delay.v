129e2ba12a559de938738703b097e352e214c3a1
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2019-10-28 21:31:09 -0700

    Signaltap shows activity on cam_vsync. Free-running clock, apparently (independent of cam_rclk)

---
+++ alt_reset_delay.v
@@ -0,0 +1,63 @@
+////////////////////////////////////////////////////////////////////////////
+// Copyright 2019 Intel Corporation. 
+//
+// This reference design file is subject licensed to you by the terms and 
+// conditions of the applicable License Terms and Conditions for Hardware 
+// Reference Designs and/or Design Examples (either as signed by you or 
+// found at https://www.altera.com/common/legal/leg-license_agreement.html ).  
+//
+// As stated in the license, you agree to only use this reference design 
+// solely in conjunction with Intel FPGAs or Intel CPLDs.  
+//
+// THE REFERENCE DESIGN IS PROVIDED "AS IS" WITHOUT ANY EXPRESS OR IMPLIED
+// WARRANTY OF ANY KIND INCLUDING WARRANTIES OF MERCHANTABILITY, 
+// NONINFRINGEMENT, OR FITNESS FOR A PARTICULAR PURPOSE. Intel does not 
+// warrant or assume responsibility for the accuracy or completeness of any
+// information, links or other items within the Reference Design and any 
+// accompanying materials.
+//
+// In the event that you do not agree with such terms and conditions, do not
+// use the reference design file.
+////////////////////////////////////////////////////////////////////////////
+
+`timescale 1 ps / 1 ps
+
+// DESCRIPTION
+//
+// This is a more elaborate version of aclr_filter, typically used for 
+// bringing up SERDES pins or PLLs. When the input ready condition is 
+// not met the output is immediately driven to not ready. When the input
+// ready becomes true the output will become ready after a programmable delay.
+//
+
+// CONFIDENCE
+// This is used very liberally in Altera test and demo designs
+//
+
+module alt_reset_delay #(
+	parameter CNTR_BITS = 16
+)
+(
+	input clk,
+	input ready_in,
+	output ready_out
+);
+
+reg [2:0] rs_meta = 3'b0 /* synthesis preserve dont_replicate */
+/* synthesis ALTERA_ATTRIBUTE = "-name SDC_STATEMENT \"set_false_path -from [get_fanins -async *reset_delay*rs_meta\[*\]] -to [get_keepers *reset_delay*rs_meta\[*\]]\" " */;
+
+always @(posedge clk or negedge ready_in) begin
+	if (!ready_in) rs_meta <= 3'b000;
+	else rs_meta <= {rs_meta[1:0],1'b1};
+end
+wire ready_sync = rs_meta[2];
+
+reg [CNTR_BITS-1:0] cntr = {CNTR_BITS{1'b0}} /* synthesis preserve */;
+assign ready_out = cntr[CNTR_BITS-1];
+always @(posedge clk or negedge ready_sync) begin
+	if (!ready_sync) cntr <= {CNTR_BITS{1'b0}};
+	else if (!ready_out) cntr <= cntr + 1'b1;
+end
+
+endmodule
+
--- ov7670_fifo_test_top.sv
+++ ov7670_fifo_test_top.sv
@@ -42,24 +42,48 @@ module ov7670_fifo_test_top(
   output wire [9:0]LEDR
 );
 
-  localparam COUNT_LIMIT = 50_000_000;
+  localparam COUNT_LIMIT = 25_000_000;
   localparam COUNTER_W = $clog2(COUNT_LIMIT);
   logic [COUNTER_W - 1:0] count;
   logic blip = '0;
-  always @(posedge MAX10_CLK1_50) begin
-    if (count >= COUNT_LIMIT - 1) begin
+
+  logic clk25;
+  logic locked;
+  logic reset_n;
+  pll25 pll25_inst(
+    .inclk0 (MAX10_CLK1_50),
+    .c0 (clk25),
+    .locked (locked)
+  );
+
+  alt_reset_delay pll_delay(
+    .clk (clk25),
+    .ready_in (locked),
+    .ready_out (reset_n)
+  );
+
+  always @(posedge clk25 or negedge reset_n) begin
+    if (~reset_n) begin
       count <= '0;
-      blip <= ~blip;
+      blip <= '0;
     end
     else begin
-      count <= count + 1'b1;
+      if (count >= COUNT_LIMIT - 1) begin
+        count <= '0;
+        blip <= ~blip;
+      end
+      else begin
+        count <= count + 1'b1;
+      end
     end
   end
 	  
-  assign cam_reset_n = 1'b1;
-  assign cam_pwdn = 1'b1; // camera is powered down
+  assign cam_reset_n = reset_n;
+  assign cam_pwdn = ~reset_n; // camera is powered down
+  // assign cam_rclk = clk25;
   assign cam_rclk = 1'b0;
-  assign cam_we = 1'b0;
+  assign cam_we = 1'b1;
+
   assign cam_oe_n = 1'b1;
   assign cam_wrst_n = 1'b1;
   assign cam_rrst_n = 1'b1;
@@ -85,7 +109,7 @@ module ov7670_fifo_test_top(
   assign HEX0 = ~(8'h3f | {blip, 7'b0}); // 0
 
   // LEDR is active high
-  assign LEDR = '0;
+  assign LEDR = {9'b0, cam_vsync};
 endmodule
 
 `default_nettype wire
